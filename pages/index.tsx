52c7ccc0bf1934baa93780ad15ca5fab0afc4590
Ethan Tyler <38672607+zonedv2@users.noreply.github.com>
2023-01-20 19:56:23 +0000

    updated projects with images

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,36 @@ import Title from '../components/home/title'
 import Project from '../components/home/project'
 
 export default function Home() {
-  const jacket = {
-    name: 'Ukiyo-e Style Jacket',
-    images: ['/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg', '/images/jacket/1.jpg','/images/jacket/1.jpg']
-  }
-  const jeans = {
-    name: 'Ukiyo-e Style Jeans',
-    images: ['/images/jacket/1.jpg','/images/jacket/1.jpg']
-  }
+  const projects = [
+    {
+      name: 'AVIAN',
+      images: ['/images/avian/1.jpg','/images/avian/2.jpg','/images/avian/3.jpg','/images/avian/4.jpg','/images/avian/5.jpg','/images/avian/6.jpg','/images/avian/7.jpg','/images/avian/8.jpg','/images/avian/9.jpg','/images/avian/10.jpg','/images/avian/11.jpg','/images/avian/12.jpg','/images/avian/13.jpg','/images/avian/14.jpg']
+    },
+    {
+      name: 'UKIYO-E / ANIME',
+      images: ['/images/ukiyo/1.jpg','/images/ukiyo/2.jpg', '/images/ukiyo/3.jpg','/images/ukiyo/4.jpg','/images/ukiyo/5.jpg','/images/ukiyo/6.jpg','/images/ukiyo/7.jpg','/images/ukiyo/8.jpg','/images/ukiyo/9.jpg','/images/ukiyo/10.jpg',]
+    },
+    {
+      name: 'BATTLE SCARRED',
+      images: ['/images/battle/1.jpg','/images/battle/2.jpg','/images/battle/3.jpg','/images/battle/4.jpg']
+    },
+    {
+      name: 'TOTES',
+      images: ['/images/totes/1.jpg','/images/totes/2.jpg','/images/totes/3.jpg','/images/totes/4.jpg','/images/totes/5.jpg','/images/totes/6.jpg','/images/totes/7.jpg']
+    },
+    {
+      name: 'ORIGARMS',
+      images: ['/images/origarms/1.jpg','/images/origarms/2.jpg']
+    },
+    {
+      name: 'ONE OFF TEES',
+      images: ['/images/tees/1.jpg','/images/tees/2.jpg','/images/tees/3.jpg','/images/tees/4.jpg','/images/tees/5.jpg']
+    },
+    {
+      name: 'EARLY PIECES',
+      images: ['/images/old/1.jpg','/images/old/2.jpg','/images/old/3.jpg','/images/old/4.jpg']
+    }
+  ]
   return (
     <>
       <Head>
@@ -25,9 +47,9 @@ export default function Home() {
       </Head>
       <Navbar />
       <Title />
-      <Project project={jacket}/>
-      <Project project={jeans}/>
-      <Project project={jacket}/>
+      {projects.map((project, index) => (
+        <Project project={project} key={index} />
+      ))}
     </>
   )
 }
